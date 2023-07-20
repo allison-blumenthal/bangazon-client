07beafbf1cd088d740aa45d69420c77c2fcd7853
Allison Blumenthal <blumen.allison@gmail.com>
2023-07-19 21:27:42 -0400

    tweaked index page for displayed user info

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useAuth } from '../utils/context/authContext';
 
 function Home() {
   const { user } = useAuth();
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -14,8 +15,8 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
+      <h1>Hello {user.username}! </h1>
+      <img src={user.profile_image_url} alt="user profile" />
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
