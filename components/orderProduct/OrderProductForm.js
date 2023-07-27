f962daa480c7607ccdf40b2cab0da69f4aa37519
Allison Blumenthal <blumen.allison@gmail.com>
2023-07-27 19:52:13 -0400

    created form to use for add to cart button

---
+++ components/orderProduct/OrderProductForm.js
@@ -0,0 +1,102 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { createOrderProduct, updateOrderProduct } from '../../utils/data/orderProductData';
+import { getOpenOrderByUserId } from '../../utils/data/orderData';
+
+const initialState = {
+  orderId: '',
+  productId: '',
+  quantity: 0,
+};
+
+function OrderProductForm({ orderProductObj, productObj }) {
+  const [openOrder, setOpenOrder] = useState({});
+  const [currentOrderProduct, setCurrentOrderProduct] = useState(initialState);
+  const { user } = useAuth();
+  const { router } = useRouter();
+
+  // function to get the open order of the user that is logged in
+  const getOpenOrder = async () => {
+    try {
+      const order = await getOpenOrderByUserId(user.id);
+      setOpenOrder(order);
+    } catch (error) {
+      console.error('Error fetching open order: ', error);
+    }
+  };
+
+  // call the function to get access to the info
+  useEffect(() => {
+    getOpenOrder();
+  }, [user.id]);
+
+  useEffect(() => {
+    if (orderProductObj.id) {
+      setCurrentOrderProduct({
+        id: orderProductObj.id,
+        orderId: orderProductObj.order_id,
+        productId: orderProductObj.product_id,
+        quantity: orderProductObj.quantity,
+      });
+    }
+  }, [orderProductObj]);
+
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setCurrentOrderProduct((prevState) => ({
+  //     ...prevState,
+  //     [name]: value,
+  //   }));
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (orderProductObj.id) {
+      let Qty = Number(currentOrderProduct.quantity);
+
+      const updatedOrderProduct = {
+        id: orderProductObj.id,
+        orderId: Number(currentOrderProduct.orderId),
+        productId: Number(currentOrderProduct.productId),
+        quantity: (Qty += 1),
+      };
+      updateOrderProduct(updatedOrderProduct).then(() => router.push('/cart'));
+    } else {
+      const orderProduct = {
+        orderId: Number(openOrder.id),
+        productId: Number(productObj.id),
+        quantity: 1,
+      };
+      createOrderProduct(orderProduct).then(() => router.push('/cart'));
+    }
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit} />
+    </>
+  );
+}
+
+OrderProductForm.propTypes = {
+  orderProductObj: PropTypes.shape({
+    id: PropTypes.number,
+    order_id: PropTypes.number,
+    product_id: PropTypes.number,
+    quantity: PropTypes.number,
+  }),
+  productObj: PropTypes.shape({
+    id: PropTypes.number,
+  }).isRequired,
+};
+
+OrderProductForm.defaultProps = {
+  orderProductObj: initialState,
+};
+
+export default OrderProductForm;
