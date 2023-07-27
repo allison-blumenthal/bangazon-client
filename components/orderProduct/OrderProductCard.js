3c0c574d9c420d04fa39335de6ad65bc43d03890
Allison Blumenthal <blumen.allison@gmail.com>
2023-07-26 23:23:24 -0400

    added orderProduct Card, updated info on order details page

---
+++ components/orderProduct/OrderProductCard.js
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Button } from 'react-bootstrap';
+import Link from 'next/link';
+
+// eslint-disable-next-line no-unused-vars
+function OrderProductCard({ orderProductObj, onUpdate }) {
+  return (
+    <Card className="text-center orderProduct-card" style={{ width: '18rem', margin: '10px' }}>
+      <Card.Header>{orderProductObj.product_id.name}</Card.Header>
+      <Card.Body>
+        <Card.Img src={orderProductObj.product_id.product_image_url} alt="product" />
+        <h2>{orderProductObj.product_id.price}</h2>
+        <h3>{orderProductObj.quantity}</h3>
+      </Card.Body>
+      <Card.Footer className="text-muted">Seller: {orderProductObj.product_id.seller_id.username}</Card.Footer>
+      <div>
+        <Link href={`/products/${orderProductObj.product_id.id}`} passHref>
+          <Button type="button" className="m-2">View</Button>
+        </Link>
+      </div>
+    </Card>
+  );
+}
+
+OrderProductCard.propTypes = {
+  orderProductObj: PropTypes.shape({
+    id: PropTypes.number,
+    quantity: PropTypes.number,
+    product_id: PropTypes.shape({
+      id: PropTypes.number,
+      seller_id: PropTypes.shape({
+        username: PropTypes.string,
+      }),
+      name: PropTypes.string,
+      price: PropTypes.number,
+      product_image_url: PropTypes.string,
+    }),
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default OrderProductCard;
