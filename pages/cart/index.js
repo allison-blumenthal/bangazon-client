feab418d58dfd311d62354372b6f699a7293c286
Allison Blumenthal <blumen.allison@gmail.com>
2023-07-27 14:06:50 -0400

    created cart item card

---
+++ pages/cart/index.js
@@ -0,0 +1,7 @@
+// import React, { useEffect, useState } from 'react';
+// import { useAuth } from '../../utils/context/authContext';
+
+// export default function ViewCart() {
+//   const [cartProducts, setCartProducts] = useState({});
+//   const { user } = useAuth();
+// }
