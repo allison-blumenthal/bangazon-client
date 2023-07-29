a971b1467704fb132c9d428ce6ac53d42c3fed76
Allison Blumenthal <blumen.allison@gmail.com>
2023-07-29 09:50:03 -0400

    sucessfully rendering the first three products in each category with clickable link

--- components/category/CategoryCard.js
+++ components/category/CategoryCard.js
@@ -1,15 +1,37 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
+import { getProductsByCategoryId } from '../../utils/data/productData';
+import CategoryProductCard from './CategoryProductCard';
 
-function CategoryCard({ categoryObj }) {
+export default function CategoryCard({ categoryObj }) {
+  const [categoryProducts, setCategoryProducts] = useState([]);
+
+  const getFirstThreeProducts = async () => {
+    try {
+      const products = await getProductsByCategoryId(categoryObj.id);
+      const three = products.slice(0, 3);
+      console.warn(three);
+      setCategoryProducts(three);
+    } catch (error) {
+      console.error('Error fetching category products: ', error);
+    }
+  };
+
+  useEffect(() => {
+    getFirstThreeProducts();
+  }, []);
+
   return (
     <Card className="text-center category-card" style={{ width: '18rem', margin: '10px' }}>
       <Card.Header>{categoryObj.label}</Card.Header>
-      <Card.Body>
-        <h6>Product 1</h6>
-        <h6>Product 2</h6>
-        <h6>Product 3</h6>
+      <Card.Body className="three-products">
+        {categoryProducts.map((categoryProduct) => (
+          <section key={`categoryProduct--${categoryProduct.id}`} className="category-products">
+            <CategoryProductCard categoryProductObj={categoryProduct} onUpdate={getFirstThreeProducts} />
+          </section>
+        ))}
       </Card.Body>
     </Card>
   );
@@ -21,5 +43,3 @@ CategoryCard.propTypes = {
     label: PropTypes.string,
   }).isRequired,
 };
-
-export default CategoryCard;
